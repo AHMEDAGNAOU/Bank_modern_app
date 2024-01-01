5d52f83a174364a68b5b968c0b8f46e2329ca0f0
AHMEDAGNAOU <72224528+AHMEDAGNAOU@users.noreply.github.com>
2024-01-01 15:16:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './style';
+
+const App = () => (
+  
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={'${styles.paddingX} ${styles.flexCenter}'}>
+        <dir className={'${styles.boxWidth}'}>
+          Navbar
+        </dir>
+      </div>
+
+      <div className={'bg-primary ${styles.flexStart}'}>
+        <div className={'${styles.boxWidth}'}>
+          Hero
+        </div>
+      </div>
+      <div className={'bg-primary ${styles.paddingX} ${styles.flexStart}'}>
+        <div className={'${styles.boxWidth}'}>
+          Stats
+          Business
+          billing
+          cardDeal
+          Testimonials 
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
+    </div>
+  
+)
+
+export default App
